d551a656c9da22d15920f32342734d54c545ddce
Aiden-BachDuong-ICD201-01 <aiden.bachduong@student.tdsb.on.ca>
2024-10-16 13:56:17 +0000

    jbhjhjjhj

--- PP2_3.py
+++ PP2_3.py
@@ -2,15 +2,30 @@
 
 def q1(): 
   #Write Assignment code here
-
+  word = input("In: ")
+  if word [-1:] == "y":
+    print("-ies")
+  elif word [-2:] == "ey":
+    print("-eys")
+  elif word [-3:] == "ife":
+    print("-ives")
+  else:
+    print("-s")
 
 def q2(): 
   #Write Assignment code here
-
+  num = int(input("In: "))
+  if num >= 1:
+    print(f"{num} is positive")
+  elif num <= -1:
+    print(f"{num} is negative")
+  else:
+    print()
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
-
+'''
 q1()
 q2()
+'''
