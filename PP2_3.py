00e3fa62779b4bf9987455754fa11be5ffa8b38a
Aiden-BachDuong-ICD201-01 <aiden.bachduong@student.tdsb.on.ca>
2024-10-17 19:43:36 +0000

    kjhgfgyuhg

--- PP2_3.py
+++ PP2_3.py
@@ -1,14 +1,21 @@
+'''
 
+    Lesson: Else If Assignment
+    Author: Aiden
+    Date Created: Oct 16, 2024
+    Date Last Modified: Oct 16, 2024
+
+'''
 
 def q1(): 
   #Write Assignment code here
   word = input("In: ")
-  if word [-1:] == "y":
-    print("-ies")
-  elif word [-2:] == "ey":
-    print("-eys")
-  elif word [-3:] == "ife":
+  if word.endswith("ife"):
     print("-ives")
+  elif word.endswith("ey"):
+    print("-eys")
+  elif word.endswith("y"):
+    print("-ies")
   else:
     print("-s")
 
@@ -19,13 +26,26 @@ def q2():
     print(f"{num} is positive")
   elif num <= -1:
     print(f"{num} is negative")
-  else:
-    print()
+ 
 
+def q3():
+  #Write Assignment code here
+  num1 = float(input("Input a number: "))
+  num2 = float(input("Input a number: "))
+  num3 = float(input("Input a number: "))
+  if num1 + num2 <= num3:
+    print("No Triangle")
+  elif num1 == num2 == num3:
+    print("Equilateral")
+  elif num1 != num2 != num3:
+    print("Scalene")
+  else:
+    print("Isosceles")
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 '''
 q1()
 q2()
+q3()
 '''
